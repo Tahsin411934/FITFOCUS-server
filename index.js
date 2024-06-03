14befb02d87bcbf83c704497a3f5cfb6473483bd
Tahsin411934 <tahsin.mail.0503@gmail.com>
2024-06-04 01:54:27 +0600

    trainer data post and get functionality added

---
+++ index.js
@@ -0,0 +1,84 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const feature = require('./Features.json');
+
+
+// MIDDLEWARE
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://Fitness-Tracker-Project:0TkmXKHJoSThlXSD@cluster0.2vutuar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const TainerDB = client.db('Fitness-Tracker-Project').collection('trainer');
+
+    app.get("/trainers",async(req,res)=>{
+      const find= TainerDB.find()
+      const result =await find.toArray()
+      res.send(result)
+    })
+
+    app.get("/trainers/:id", async(req,res)=>{
+      const id= req.params.id;
+      const quary = {_id: new ObjectId(id)}
+      const result = await TainerDB.findOne(quary)
+      res.send(result)
+    })
+    app.post("/trainers", async(req,res)=>{
+      const trainer = req.body;   
+      const result = await TainerDB.insertOne(trainer)
+      res.send(result)
+    })
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+app.get('/features', async(req,res)=>{
+    res.send(feature)
+})
+
+
+
+
+
+
+
+
+
+
+app.get("/", (req,res)=>{
+    res.send('server is running');
+})
+
+app.listen(port, ()=>
+    console.log('running port is ' , port )
+)
